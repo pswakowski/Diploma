dfab274ad419ee4c218a78074860eae55a4026fc
unknown <pswakowski@gmail.com>
2019-01-27 15:03:38 +0100

    fixed worktime one more time

--- classes/Helpers.php
+++ classes/Helpers.php
@@ -79,18 +79,25 @@ class Helpers
         $start_working_day = new DateTime("07:00:00");
         $end_working_day = new DateTime("18:00:00");
 
-        if ($date_now > $end_working_day)
+        $interval = $date_now->diff($date_from_base);
+
+        if ($interval->days == 0)
         {
-            return $end_working_day->diff($date_from_base)->format("%h godz. %i min.");
-        }
-        else if ($date_now < $start_working_day)
+            if ($date_now > $end_working_day) {
+                return $end_working_day->diff($date_from_base)->format("%d%h godz. %i min.");
+            }
+            else if ($date_now < $start_working_day)
+            {
+                $zero_hours = new DateTime("00:00:00");
+                return $zero_hours->format("H:i:s");
+            }  else
+            {
+                return $date_from_base->diff($date_now)->format("%h godz. %i min.");
+            }
+        } else
         {
             $zero_hours = new DateTime("00:00:00");
             return $zero_hours->format("H:i:s");
-        }
-        else
-        {
-            return $date_from_base->diff($date_now)->format("%h godz. %i min.");
         }
     }
 }
